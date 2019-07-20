d410081d82359cfbb61bfa72bd27bb38d521465d
Julian Colon <colonj22@gmail.com>
2019-07-20 15:53:43 -0400

    Initial Commit

---
+++ Quick Med/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quick Med
+//
+//  Created by Julian Colon on 7/20/19.
+//  Copyright © 2019 Julian Colon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
